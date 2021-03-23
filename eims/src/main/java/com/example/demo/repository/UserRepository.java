28ef6639ffe4af7ff949d3149b0b6ea4fa36199c
kaminoha <80882661+kaminoha@users.noreply.github.com>
2021-03-23 15:19:01 +0800

    Add files via upload

--- eims/src/main/java/com/example/demo/repository/UserRepository.java
+++ eims/src/main/java/com/example/demo/repository/UserRepository.java
@@ -3,11 +3,14 @@ package com.example.demo.repository;
 import java.util.List;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
 import com.example.demo.model.Users;
 
 @Repository
 public interface UserRepository extends JpaRepository<Users, Long>{
+	@Query(value = "select * from users where username = '?1' and pass = '?2'",
+			nativeQuery = true)
 	List<Users> findByUserNameAndPassword(String username, String password);
 }
