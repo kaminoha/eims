4984397ed7b54e7d553c0fbb8240c84666c71f30
kaminoha <80882661+kaminoha@users.noreply.github.com>
2021-03-27 17:58:20 +0800

    Add files via upload

--- eims/src/main/java/com/example/demo/repository/DBFileRepository.java
+++ eims/src/main/java/com/example/demo/repository/DBFileRepository.java
@@ -4,15 +4,16 @@ import java.util.List;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 import com.example.demo.model.Files;
 
 @Repository
 public interface DBFileRepository extends JpaRepository<Files, String>{
-	@Query(value = "Select * from files where file_id = '?1'",
+	@Query(value = "Select * from files where file_id = :fileId",
 			nativeQuery = true)
-	Files findById(String fileId);
+	Files findById(@Param("fileId") String fileId);
 	@Query(value = "Select * from files",
 			nativeQuery = true)
 	List<Files>findFiles();
