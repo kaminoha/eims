1b1f92a7d7fba1b0ea3cc27d43fc09cbdd25e38a
kaminoha <80882661+kaminoha@users.noreply.github.com>
2021-05-08 14:23:02 +0800

    Add files via upload

---
+++ eims/src/main/java/com/example/demo/model/BreakDown.java
@@ -0,0 +1,85 @@
+package com.example.demo.model;
+
+import java.io.Serializable;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.ManyToOne;
+
+import com.fasterxml.jackson.annotation.JsonBackReference;
+
+@Entity
+public class BreakDown implements Serializable{
+
+	private static final long serialVersionUID = 1L;
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.AUTO)
+	private Long breakDownId;
+	
+	@Column(name = "item")
+	private String productName;
+	
+	@Column(name = "cost")
+	private float price;
+	
+	private int quantity;
+	
+	private float total;
+	
+	@ManyToOne
+	@JsonBackReference
+	private Customer customer;
+
+	public Long getBreakDownId() {
+		return breakDownId;
+	}
+
+	public void setBreakDownId(Long breakDownId) {
+		this.breakDownId = breakDownId;
+	}
+
+	public String getProductName() {
+		return productName;
+	}
+
+	public void setProductName(String productName) {
+		this.productName = productName;
+	}
+
+	public float getPrice() {
+		return price;
+	}
+
+	public void setPrice(float price) {
+		this.price = price;
+	}
+
+	public int getQuantity() {
+		return quantity;
+	}
+
+	public void setQuantity(int quantity) {
+		this.quantity = quantity;
+	}
+
+	public float getTotal() {
+		return total;
+	}
+
+	public void setTotal(float total) {
+		this.total = total;
+	}
+
+	public Customer getCustomer() {
+		return customer;
+	}
+
+	public void setCustomer(Customer customer) {
+		this.customer = customer;
+	}
+	
+}
