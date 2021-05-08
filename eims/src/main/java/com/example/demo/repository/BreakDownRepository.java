bd6d654f3116edc1db41d05bdd5fd09a9535f7f9
kaminoha <80882661+kaminoha@users.noreply.github.com>
2021-05-08 14:22:22 +0800

    Add files via upload

---
+++ eims/src/main/java/com/example/demo/repository/BreakDownRepository.java
@@ -0,0 +1,11 @@
+package com.example.demo.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import com.example.demo.model.BreakDown;
+
+@Repository
+public interface BreakDownRepository extends JpaRepository<BreakDown, Long>{
+
+}
