62cce8d0ef95381f8a4fb8bedf7eb9e297c768bf
kaminoha <80882661+kaminoha@users.noreply.github.com>
2021-05-15 14:15:45 +0800

    Add files via upload

--- eims/src/main/java/com/example/demo/model/BreakDown.java
+++ eims/src/main/java/com/example/demo/model/BreakDown.java
@@ -10,6 +10,8 @@ import javax.persistence.Id;
 import javax.persistence.JoinColumn;
 import javax.persistence.ManyToOne;
 
+import com.fasterxml.jackson.annotation.JsonBackReference;
+
 @Entity
 public class BreakDown implements Serializable{
 
@@ -31,6 +33,7 @@ public class BreakDown implements Serializable{
 	private float total;
 	
 	@ManyToOne
+	@JsonBackReference
 	@JoinColumn(name = "finance_id")
 	private Finance finance;
 
