670c1c2eabcc78deba8b5c8f40fcf937231b0c16
kaminoha <80882661+kaminoha@users.noreply.github.com>
2021-05-15 13:50:45 +0800

    Add files via upload

--- eims/src/main/java/com/example/demo/repository/BreakDownRepository.java
+++ eims/src/main/java/com/example/demo/repository/BreakDownRepository.java
@@ -11,8 +11,10 @@ import com.example.demo.model.BreakDown;
 
 @Repository
 public interface BreakDownRepository extends JpaRepository<BreakDown, Long>{
-	@Query(value = "select * from break_down as b left join finance as f "
+/*	@Query(value = "select * from break_down as b left join finance as f "
 			+ "ON b.finance_id = f.finance_id where b.finance_id = :financeId",
 			nativeQuery = true)
-	List<BreakDown> findByFinanceId(@Param("financeId") Long financeId);
+	List<BreakDown> findByFinanceId(@Param("financeId") Long financeId);*/
+	
+	List<BreakDown> findAllByFinanceFinanceId(Long financeId);
 }
